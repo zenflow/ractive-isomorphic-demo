25d27737cf5431e8291770ef4075f33236ca2c60
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-05-13 17:45:38 -0400

    put polygons top-to-bottom in stacked mode instead of bottom-to-top

--- shared/Site/components/Graph.js
+++ shared/Site/components/Graph.js
@@ -27,39 +27,36 @@ var Graph = ri.Ractive.extend({
 		ri._.forEach(data, function(layer_data){
 			if (layer_data.length != data[0].length){throw new Error('must have equal number of data points for each layer!')}
 		});
-		var max;
-		if (relative) {
-			max = ri._.map(data[0], function(datum, x){
+		var whole = (relative && ri._.map(data[0], function(datum, x){
 				return ri._.sum(ri._.map(data, function(layer_data, i){return data[i][x];})) || 1;
-			});
-		} else {
-			if (stacked){
-				max = ri._.max(ri._.map(ri._.range(data[0].length), function (x) {
-					return ri._.sum(ri._.map(data, function (layer_data) {
-						return layer_data[x];
-					}))
-				}));
-			} else {
-				max = ri._.max(ri._.map(data, function (layer_data) {
-					return ri._.max(layer_data);
-				}));
-			}
-			max = max || 1;
-		}
+			}))
+			|| (stacked && ri._.max(ri._.map(data[0], function (datum, x) {
+				return ri._.sum(ri._.map(data, function (layer_data) {return layer_data[x];}));
+			})))
+			|| ri._.max(ri._.map(data, function (layer_data) {
+				return ri._.max(layer_data);
+			}))
+			|| 1;
+		var empty = (stacked && relative && repeat(0, data[0].length))
+			|| (stacked && !relative && ri._.map(data[0], function(datum, x){
+				return whole - ri._.sum(ri._.map(data, function (layer_data, i) {return layer_data[x];}));
+			}));
 		var lines = [];
 		ri._.forEach(data, function(layer_data, i){
 			lines.push(ri._.map(layer_data, function(datum, x){
+				var _whole = relative ? whole[x] : whole;
+				var portion = height * datum / _whole;
 				return {
 					x: width * x / (data[i].length - 1),
-					y: (stacked && i ? lines[i-1][x].y : 0) - (height * datum / (relative ? max[x] : max))
+					y: (stacked ? (i ? lines[i-1][x].y : -height + (height*empty[x]/_whole)) + portion : -portion)
 				};
 			}));
 		});
 		return ri._.map(lines, function(line, i){
 			var line_b = !stacked && line.slice()
 				|| i && lines[i-1].slice()
-				|| ri._.map(line, function(p, x){return {x: p.x, y: 0}});
-			return [].concat(line, line_b.reverse());
+				|| ri._.map(line, function(p, x){return {x: p.x, y: -height + (height*empty[x]/(relative ? whole[x] : whole))}});
+			return line.concat(line_b.reverse());
 		});
 	},
 	onconfig: function(){
@@ -71,33 +68,39 @@ var Graph = ri.Ractive.extend({
 		var self = this;
 		self.observe('data stacked relative width height', function(){
 			// animate old polygons to new ones
-			var polygons = self.getPolygons();
+			var new_polygons = self.getPolygons();
 			if (self.root.on_client){
-				// animate new/old polygons in from/out to horizontal line along top
-				var old_polygons = self.get('polygons');
-				var actual_polygon_length = polygons.length;
-				if (old_polygons.length != polygons.length){
+				// animate new/old polygons in from/out to horizontal line along bottom
+				var trans_polygons = new_polygons;
+				var old_polygons = self.get('polygons').slice();
+				if (old_polygons.length != new_polygons.length){
 					var width = self.get('width');
 					var height = self.get('height');
-					var empty = ri._.map(ri._.range(Math.abs(old_polygons.length - polygons.length)), function(i){
-						var line = ri._.map(ri._.range(polygons[0].length / 2), function(x){
-							return {x: width * x / (polygons[0].length / 2 - 1), y: -height};
-						});
-						return line.slice().concat(line.reverse());
+					var empty_line = ri._.map(ri._.range(new_polygons[0].length / 2), function(x){
+						return {x: width * x / (new_polygons[0].length / 2 - 1), y: 0};
 					});
-					if (old_polygons.length < polygons.length){
-						self.set('polygons', old_polygons.concat(empty));
+					var empty = empty_line.concat(empty_line.slice().reverse());
+					var empties = repeat(empty, Math.abs(old_polygons.length - new_polygons.length));
+					if (old_polygons.length < new_polygons.length){
+						self.set('polygons', old_polygons.concat(empties));
 					} else {
-						polygons = polygons.concat(empty);
+						trans_polygons = new_polygons.concat(empties);
 					}
+				} else {
 				}
-				self.animate('polygons', polygons).then(function(){
-					self.set('polygons', self.get('polygons').slice(0, actual_polygon_length));
+				self.animate('polygons', trans_polygons).then(function(){
+					if (old_polygons.length != new_polygons.length) {
+						self.set('polygons', new_polygons);
+					}
 				});
 			} else {
-				self.set('polygons', polygons);
+				self.set('polygons', new_polygons);
 			}
 		}, {init: false});
 	}
 });
+
+function repeat(obj, length){
+	return ri._.map(ri._.range(length), function(i){return obj;});
+}
 module.exports = Graph;
